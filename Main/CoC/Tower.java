a4544d093b1ca7cddfb9108df6c0ba1bbc77ee89
Calvin Vuong <cvuong@stuy.edu>
2016-05-31 18:13:11 -0400

    renamed files to allow for easy text editing

--- Main/CoC/Tower.java
+++ Main/CoC/Tower.java
@@ -1,18 +1,18 @@
 public class Tower extends Unit{
   int  _hp;
-  int _mp;
   int _xcor;
   int _ycor;
-  public Tower(int xcor, int ycor){
+
+   public Tower(int xcor, int ycor){
     _xcor = xcor;
     _ycor = ycor;
     _hp = 100;
-    _mp = 0;
   }
-  int attack(){
-    return 7;
-  }
+
+   public takeDamage(int n) {
+   	  
   void draw(){
     rect(_xcor*100, _ycor*100 , 10, 10);
   }
+
 }
