51add812ef8f34a2a78bc3b0dfb7f03fb09328dc
Calvin Vuong <cvuong@stuy.edu>
2016-06-01 11:29:25 -0400

    all basic defense tower methods written; used PRIORITY QUEUES

--- Main/CoC/Defense.java
+++ Main/CoC/Defense.java
@@ -17,6 +17,16 @@ public class Defense{
 	_monstersToShoot = new ArrayPriorityQueue<Monsters>();
     }
     
+    // determines exactly how to attack monsters (e.g. individual damage, collateral damage, etc.)
+    public void shoot() {
+	// remove left-over dead monsters from queue
+	while ( ! _monstersToShoot.peekTop().isAlive() )
+	    _monstersToShoot.removeTop();
+
+	Monster target = _monstersToShoot.peekTop();
+	attack(target);
+    }
+	
     // deals damage to enemy equal to damage instance var, with crit chance factored in
     // returns true if the enemy is dead after attack
     public boolean attack(Monster enemy){
@@ -26,6 +36,13 @@ public class Defense{
 	    return enemy.takeDamage(_attackPower);
     }
     
+    // queues Monsters into things to shoot for this tower if not already in queue and in radius
+    public void queueMonsters(List<Monster> monsterList) {
+	for ( Monster monster : monsterList ) {
+	    if ( inRadius(monster) && ! monsterList.contains(monster) )
+		_monstersToShoot.add(monster);
+	}
+    }
     
     // returns true if enemy is in shoot radius of the tower
     public boolean inRadius(Monster enemy) {
